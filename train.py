b0e538c049a852cb4b818bd58cfc7ca2bd51f869
asiskumarroy <asisroylitun@gmail.com>
2020-07-19 02:04:12 +0530

    prabable final version

---
+++ train.py
@@ -0,0 +1,165 @@
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import torchvision
+from torchvision import datasets,models,transforms
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import os
+import time
+import copy
+
+plt.ion()
+
+print(torch.cuda.is_available())
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'valid': transforms.Compose([
+        transforms.Resize(226),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+data_dir = 'data'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'valid']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                             shuffle=True, num_workers=4)
+              for x in ['train', 'valid']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'valid']}
+class_names = image_datasets['train'].classes
+
+device = torch.device('cpu')
+print(device)
+
+print ("train dataset size is {} and valid dataset size is {}".format(dataset_sizes['train'],dataset_sizes['valid']))
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+
+# Get a batch of training data
+inputs, classes = next(iter(dataloaders['train']))
+
+# Make a grid from batch
+out = torchvision.utils.make_grid(inputs)
+
+imshow(out, title=[class_names[x] for x in classes])
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'valid']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'valid':
+                scheduler.step(loss)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'valid' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+model_ts=models.resnet152(pretrained=True)
+print(model_ts)
+
+num_ftrs=model_ts.fc.in_features
+model_ts.fc=nn.Linear(num_ftrs,2,bias=True)
+print(model_ts)
+
+for index,param in enumerate(model_ts.parameters()):
+    if index < 353:
+        param.requires_grad=False
+
+model_ts = model_ts.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+optimizer = optim.SGD(model_ts.parameters(), lr=0.001, momentum=0.9,nesterov=True)
+
+exp_lr_scheduler = lr_scheduler.ReduceLROnPlateau(optimizer,mode='min',
+                                                  factor=0.5, patience=1, verbose=True,
+                                                  threshold=0.0001, threshold_mode='rel',
+                                                  cooldown=0, min_lr=0, eps=1e-08)
+
+model_ts = train_model(model_ts, criterion, optimizer, exp_lr_scheduler,
+                       num_epochs=35)
+
+torch.save(model_ts,'trained_model.pt' )
